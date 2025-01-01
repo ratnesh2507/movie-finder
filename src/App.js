5c540583c583211666c59645d508942354826a93
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-01-01 17:34:10 +0530

    Split up the navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import StarRating from "./StarRating";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import NumResults from "./components/NumResults";
 
 export default function App() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   return (
-    <div>
-      <StarRating />
-    </div>
+    <>
+      <Navbar>
+        <Search query={query} setQuery={setQuery} />
+        <NumResults />
+      </Navbar>
+    </>
   );
 }
